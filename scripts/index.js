72aba0aa96c0dd3e1d95981a2bb1d5b4b7afa701
Irina Tyurina <ityu.rn@gmail.com>
2020-09-14 16:00:19 +0300

    добавить карточки через темплейт

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//попап для редактирования профиля:
 const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-btn');
 const popupCloseButton = document.querySelector('.popup__close');
@@ -35,5 +36,50 @@ const formSubmit = (event) => {
 popupOpenButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
 popupSaveButton.addEventListener('submit', formSubmit);
+
+//массив с контентом для карточек:
+const initialCards = [
+    {
+        name: 'Кольский район',
+        link: 'https://res.cloudinary.com/ityurina/image/upload/v1599936042/mesto/teriberka01_cmrrfe.jpg'
+    },
+    {
+        name: 'Кольский район',
+        link: 'https://res.cloudinary.com/ityurina/image/upload/v1599936043/mesto/teriberka02_yl1qpz.jpg'
+    },
+    {
+        name: 'Кольский район',
+        link: 'https://res.cloudinary.com/ityurina/image/upload/v1599936042/mesto/teriberka03_ne5uwa.jpg'
+    },
+    {
+        name: 'Териберка',
+        link: 'https://res.cloudinary.com/ityurina/image/upload/v1599936042/mesto/teriberka04_brc7tr.jpg'
+    },
+    {
+        name: 'Териберка',
+        link: 'https://res.cloudinary.com/ityurina/image/upload/v1599936042/mesto/teriberka05_xia8mw.jpg'
+    },
+    {
+        name: 'Териберка',
+        link: 'https://res.cloudinary.com/ityurina/image/upload/v1599936042/mesto/teriberka01_cmrrfe.jpg'
+    }
+];
+const template = document.querySelector('.template__card');
+const cards = document.querySelector('.elements__list');
+
+function render() {
+    initialCards.forEach(renderItem);
+}
+
+//копируем темплейт карточки, возвращаем данные из массива, добавляем карточки в ul:
+function renderItem({name, link}) {
+    const newCard = template.content.cloneNode(true);
+    newCard.querySelector('.elements__name').innerText = name;
+    newCard.querySelector('.elements__photo').alt = name;
+    newCard.querySelector('.elements__photo').src = link;
+    cards.appendChild(newCard);
+}
+
+render();
 
 
